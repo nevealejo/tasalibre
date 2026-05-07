dd5606b7597c29b77738752278b5f6ece69f3631
nevealejo <nevealejo@gmail.com>
2026-05-07 12:17:26 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TasaLibre from './TasaLibre.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TasaLibre />
+  </React.StrictMode>,
+)
